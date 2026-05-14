87c93d94d95b5c32fd99b0337a6d5d3aa466267d
tanvirsadik1365-cpu <tanvirsadik1365@gmail.com>
2026-05-14 21:41:04 +0100

    Final version2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ import {
 } from "@/lib/seo";
 import "./globals.css";
 
-const googleTagManagerId = "GTM-MKW7PRTP";
+const googleTagManagerId = "GTM-MGHZ7J2H";
 const googleTagManagerScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
